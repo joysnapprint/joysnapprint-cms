420fe7bf9925f9327eb77f1b476bc39682bb5a89
Christina Lutes <joysnapprint@gmail.com>
2025-08-30 14:26:13 -0700

    Fix webpack prod build: update contenthash and plugin config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,35 +1,31 @@
-const {merge} = require("webpack-merge");
-const path = require("path");
-
-const TerserPlugin = require("terser-webpack-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
-const common = require("./webpack.common.js");
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  mode: "production",
-
+  mode: 'production',
   output: {
-    filename: "[name].[hash:5].js",
-    chunkFilename: "[id].[hash:5].css",
-    path: path.resolve(__dirname, "dist")
+    filename: 'js/[name].[contenthash].js',
+    chunkFilename: 'js/[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
   },
-
   optimization: {
     minimize: true,
     minimizer: [
       new TerserPlugin({
-        terserOptions: {
-          sourceMap: true,
-        },
-        exclude: /\/node_modules\//,
-      }),
-      new MiniCssExtractPlugin({
-        filename: "[name].[hash:5].css",
-        chunkFilename: "[id].[hash:5].css"
+        extractComments: false,
       }),
       new CssMinimizerPlugin(),
-    ]
-  }
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash].css',
+      chunkFilename: 'css/[id].[contenthash].css',
+    }),
+  ],
 });
